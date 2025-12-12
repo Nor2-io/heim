6c55669129fb76a4157f0a07390a38aa2c605f17
Andreas Söderberg <andreas.soderber@nor2.io>
2025-12-12 11:14:00 -0600

    Added empty holders for documentaiton and added rust-job template

--- heim-docs/docs/astro.config.mjs
+++ heim-docs/docs/astro.config.mjs
@@ -1,53 +1,59 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/heim/docs',
-	site: 'https://cloud.heim.dev',
-	integrations: [
-		starlight({
-			title: 'Heim Docs',
-			logo: {
-				light: './src/assets/logo-light.svg',
-				dark: './src/assets/logo-dark.svg',
-				replacesTitle: true,
-			},
-			favicon: "./src/assets/favicon.png",
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/Nor2-io/heim' },
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.com/invite/heim' }
-			],
-			sidebar: [
-				{
-					label: 'Start Here',
-					autogenerate: { directory: 'start-here' },
-				},
-				{
-					label: 'Application Configuration',
-					autogenerate: { directory: 'config' },
-				},
-				{
-					label: 'Organization',
-					autogenerate: { directory: 'organization' },
-				},
-				{
-					label: 'CLI',
-					autogenerate: { directory: 'cli' },
-				},
-				{
-					label: 'Templates',
-					autogenerate: { directory: 'templates' }
-				},
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-			],
-			customCss: [
-				'./src/styles/custom.css'
-			]
-		}),
-	],
+  base: "/heim/docs",
+  site: "https://cloud.heim.dev",
+  integrations: [
+    starlight({
+      title: "Heim Docs",
+      logo: {
+        light: "./src/assets/logo-light.svg",
+        dark: "./src/assets/logo-dark.svg",
+        replacesTitle: true,
+      },
+      favicon: "./src/assets/favicon.png",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Nor2-io/heim",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.com/invite/heim",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Start Here",
+          autogenerate: { directory: "start-here" },
+        },
+        {
+          label: "Application",
+          autogenerate: { directory: "application" },
+        },
+        {
+          label: "Organization",
+          autogenerate: { directory: "organization" },
+        },
+        {
+          label: "CLI",
+          autogenerate: { directory: "cli" },
+        },
+        {
+          label: "Templates",
+          autogenerate: { directory: "templates" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
